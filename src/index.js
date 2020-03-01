0c2c6ddc4851bde230f731ecef47b02b49ae3a6e
Maksim Pryma <Maksim.Pryma@standardbank.co.za>
2020-03-01 22:45:28 +0300

    feat: add revers int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -1 * n;
+    }
+    let str = new String(n).split("");
+    let result = "";
+    for (let i = str.length - 1; i >=0; i --) {
+        result = result + str[i];
+    }
+    return result;
 }
